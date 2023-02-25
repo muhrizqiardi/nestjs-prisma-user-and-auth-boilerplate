f5eef35fafaae63e30fc28ad9766a56411821ca6
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-25 18:40:31 +0700

    feat: add `createUser` method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    return this.prisma.user.create({
+      data: { ...createUserDto, password: hashedPassword },
+    });
+  }
+}
