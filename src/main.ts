28c870dff256a2f57a479752eb12f07d32dd18ab
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-25 18:50:42 +0700

    feat: add Swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js Prisma User and Auth Boilerplate Docs')
+    .setDescription('API Docs for the Nest.js Prisma User and Auth Boilerplate')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
