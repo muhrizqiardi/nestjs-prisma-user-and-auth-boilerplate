5cbcc685cf2ab9e0883247e580f7e90b679c5c95
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-25 19:34:05 +0700

    feat: add decorators for Swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
 import { Prisma } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto implements Prisma.UserCreateInput {
+  @ApiProperty({ format: 'email' })
   email: string;
+
+  @ApiProperty({ format: 'password', minLength: 8 })
   password: string;
 }
