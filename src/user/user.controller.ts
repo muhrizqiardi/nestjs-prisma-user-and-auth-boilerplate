64c9e77350e3fa5f8350a27121f12d1dd3595878
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-26 08:55:47 +0700

    feat: added endpoints for find one, update, and delete

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Request,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
 import { UserService } from './user.service';
@@ -13,5 +31,41 @@ export class UserController {
   @ApiCreatedResponse({ type: UserEntity })
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  findOne(@Param('id', ParseIntPipe) id: number, @Request() request: any) {
+    if (request.user.userId !== id) throw new UnauthorizedException();
+
+    return this.userService.findOne({
+      id: request.user.userId,
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: UserEntity })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Request() request: any,
+    @Body() createUserDto: CreateUserDto,
+  ) {
+    if (request.user.userId !== id) throw new UnauthorizedException();
+
+    return this.userService.update(id, createUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: UserEntity })
+  delete(@Param('id', ParseIntPipe) id: number, @Request() request: any) {
+    if (request.user.userId !== id) throw new UnauthorizedException();
+
+    return this.userService.delete(id);
   }
 }
