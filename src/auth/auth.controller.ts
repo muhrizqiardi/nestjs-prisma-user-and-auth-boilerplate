226ca0dafe88c07dd9f72a39bedc0667634b148c
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-26 08:00:08 +0700

    refactor: rename to AuthLoginResponse

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiBody, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
-import { JwtPayloadEntity } from './entities/jwt-payload.entity';
+import { AuthLoginResponse } from './entities/auth-login-response.entity';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -12,7 +12,7 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @ApiBody({ type: LoginDto })
-  @ApiOkResponse({ description: 'Login successful', type: JwtPayloadEntity })
+  @ApiOkResponse({ description: 'Login successful', type: AuthLoginResponse })
   @Post()
   async login(@Request() req) {
     return this.authService.login(req.user);
--- src/auth/entities/auth-login-response.entity.ts
+++ src/auth/entities/auth-login-response.entity.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class JwtPayloadEntity {
+export class AuthLoginResponse {
   @ApiProperty()
   access_token: string;
 }
