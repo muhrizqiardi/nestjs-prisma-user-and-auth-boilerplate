f6058d20703ad22002bc8aeac52b7f2ec213c544
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-26 07:11:30 +0700

    feat: add JWT functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt/dist';
 import { JWT_SECRET } from 'src/constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -16,7 +17,7 @@ import { JWT_SECRET } from 'src/constants';
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
