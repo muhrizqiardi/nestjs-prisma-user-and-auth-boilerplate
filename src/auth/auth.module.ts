e95d63b5848b9cdcf2e41f8831ba4a110fa676fb
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-26 07:55:00 +0700

    feat: change JWT token expiration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: JWT_SECRET,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '30d' },
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
