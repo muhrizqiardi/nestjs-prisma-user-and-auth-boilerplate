557d858999d841ac27a31b3811d2f23475a87243
Muhammad Rizqi Ardiansyah <muhrizqiardi@gmail.com>
2023-02-26 07:09:38 +0700

    docs: add Bearer auth

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
 
   const config = new DocumentBuilder()
     .setTitle('Nest.js Prisma User and Auth Boilerplate Docs')
     .setDescription('API Docs for the Nest.js Prisma User and Auth Boilerplate')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
